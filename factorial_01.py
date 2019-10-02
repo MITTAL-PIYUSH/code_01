22dbc516b3efb5328f1ce68d77ae01dcf4177d88
MITTAL-PIYUSH <51044096+MITTAL-PIYUSH@users.noreply.github.com>
2019-10-02 08:39:42 +0530

    Update factorial_01.py

--- factorial_01.py
+++ factorial_01.py
@@ -3,8 +3,8 @@ def factorial(n):
     if n==1 :
         return 1
 
-    f = n* factorial(n-1) # recursive statement
+    f = n*factorial(n-1) # recursive statement
     return f 
 
-n = int(input("Enter a number :"))
+n = int(input("Enter a number :"))               #To Take a input
 print(f"Factorial of {n} : {factorial(n)}")
